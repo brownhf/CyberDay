b0ffb3bcb09f6816ab171b08466833a21df3d763
Harry Brown <brownhf@dukes.jmu.edu>
2020-11-18 18:17:38 -0500

    Volunteer remove shirt size

--- CyberDay/NewVolunteer.aspx.designer.cs
+++ CyberDay/NewVolunteer.aspx.designer.cs
@@ -211,24 +211,6 @@ namespace CyberDay {
         protected global::System.Web.UI.WebControls.RequiredFieldValidator reqPhone;
         
         /// <summary>
-        /// lblShirtSize control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblShirtSize;
-        
-        /// <summary>
-        /// ddlShirtSize control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.DropDownList ddlShirtSize;
-        
-        /// <summary>
         /// btnSaveVolunteer control.
         /// </summary>
         /// <remarks>
